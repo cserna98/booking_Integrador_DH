b69dab6b7cfcafe3511804fa68fbbd6b196ae941
rociojulio <83599905+rociojulio@users.noreply.github.com>
2023-01-19 16:11:41 -0500

    Creación de la carpeta para el backend

---
+++ SpatialBookingBackend/src/test/java/com/team7/SpatialBookingBackend/SpatialBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.SpatialBookingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpatialBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
