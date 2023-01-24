cb0ad9baf0d50b883c72965b8746349ae5d4649a
Yuri Alejandra Bermúdez Ríos <yabermudez@opti.local>
2023-01-24 01:10:41 -0500

    Modificación de funcionalidad para validar Evento Login

--- spacialbookingfront/src/components/Login.jsx
+++ spacialbookingfront/src/components/Login.jsx
@@ -4,7 +4,48 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function Login(){
+    const validationData = {
+        email: "yuri.bermudez@turia-group.com",
+        password: "DigitalHouse2"
+    }
+
     // Creación de estados por cada input
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    // Manejadores de eventos para cada input para actualziar los estados a medida que el usurio escribe en los inputs
+    const onChangeEmail = (e) => setEmail(e.target.value);
+    const onChangePassword = (e) => setPassword(e.target.value);
+
+
+    // Funcion para validar campos del Login
+    function validateLogin(){
+        if(email===validationData.email && password===validationData.password){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Manejador del evento del envío de formulario
+    const onSubmitLogin = (e) =>{
+        e.preventDefault();
+        const isCorrectLogin = validateLogin();
+        if(isCorrectLogin){
+            setEmail("");
+            setPassword("");
+            alert("Login exitoso")
+        }else{
+            alert("Error credenciales inválidas. Por favor valide los campos ingresados")
+        }
+    }
+
+
+
+
+
+
 
     return (
         <>
@@ -15,15 +56,15 @@ function Login(){
             <button>X</button>
             {/* </Link> */}
         </div>
-        <form>
+        <form onSubmit={onSubmitLogin}>
         <h2>Iniciar sesión</h2>
         <div>
         <label htmlFor="email">Correo electrónico</label>
-        <input type="email" name="email" id="email" placeholder="Ingresa el correo electrónico"></input>
+        <input type="email" name="email" id="email" placeholder="Ingresa el correo electrónico" value={email} onChange={onChangeEmail}></input>
         </div>
         <div>
         <label htmlFor="password">Contraseña</label>
-        <input type="password" name="password" id="password" placeholder="Ingresa tu contraseña"></input>
+        <input type="password" name="password" id="password" placeholder="Ingresa tu contraseña" value={password} onChange={onChangePassword}></input>
         </div>
         <button type="submit" >Ingresar</button>
         <p>¿Aún no tines cuenta? <Link to='/signup'>Regístrate</Link></p>
