e73f0256bcd21be38fde77a4b2585175b10da3c3
rociojulio <83599905+rociojulio@users.noreply.github.com>
2023-01-24 20:24:46 -0500

    creación de la tabla categorías

---
+++ ovniricSpatialBookingBackend/src/main/java/com/ovniric/model/Category.java
@@ -0,0 +1,53 @@
+package com.ovniric.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private String urlImage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
