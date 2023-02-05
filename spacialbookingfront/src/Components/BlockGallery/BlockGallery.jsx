2bf6e78699de36b63d9ee72a9ad42fb2f30e14f0
Yuri Alejandra Bermúdez Ríos <yabermudez@opti.local>
2023-02-05 17:53:49 -0500

    Organizando imágenes en Desktop

--- spacialbookingfront/src/Components/BlockGallery/BlockGallery.jsx
+++ spacialbookingfront/src/Components/BlockGallery/BlockGallery.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import imgList from "../../assets/Json/imgProducts.json";
+import styles from "./BlockGallery.module.css"
 
 function BlockGallery(){
     return(
-        <div>
-            <div><img src={`${imgList[0].url}`} alt="" /></div>
-            <div></div>
+        <div className={styles.containerImg}>
+            <div className={styles.mainImg}><img src={`${imgList[0].url}`} alt="" /></div>
+            <div className={styles.containerSecondImg}>
+                {imgList.slice(1,5).map((image) => (
+                    <img key={image.id} src={image.url} alt={image.title}></img>
+                ))}
+            </div>
         </div>
     )
 }
