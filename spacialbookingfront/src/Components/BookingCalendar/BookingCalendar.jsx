6930a165ba7c506e9f206d9c68c7bbc34ef35779
Yuri Alejandra Bermúdez Ríos <yabermudez@opti.local>
2023-02-07 00:35:50 -0500

    Funcionalidad de calendario mobile y tablet

--- spacialbookingfront/src/Components/BookingCalendar/BookingCalendar.jsx
+++ spacialbookingfront/src/Components/BookingCalendar/BookingCalendar.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import {useState} from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import styles from "./BookingCalendar.module.css"
 
 function BookingCalendar(){
+    const [value, setValue] = useState(new Date());
+
+    function onChange(nextValue){
+        setValue(nextValue);
+    }
     return(
-        <div>
-            <div><h1>Hello World</h1></div>
-            <div><p>Quieres hacer una reserva</p></div>
+        <div className={styles.calendarDouble}>
+        <h2>Fechas Disponibles</h2>
+        <div className={styles.bookingCalendar}>
+            <div className={styles.calendarSelect}>
+                <Calendar  onChange={onChange} value={value} showDoubleView={true} calendarType={"US"}>
+                </Calendar>
+            </div>
+            <div className={styles.calendarMobile}>
+                <Calendar  onChange={onChange} value={value}  calendarType={"US"}>
+                </Calendar>
+                </div>
+            <div className={styles.sectionBooking}>
+                <p className={styles.letter}>A un clic de vivir tu experiencia espacial</p>
+                <button className={styles.viewMore}>Reserva Ya!</button>
+            </div>
+        </div>
         </div>
     )
 
