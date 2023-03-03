b50148720e100274eda367f61ffb0061d1b2fd88
Yuri Alejandra Bermúdez Ríos <yabermudez@opti.local>
2023-03-03 00:58:45 -0500

    Campos para formulario de agregar productos

--- spacialbookingfront/src/Components/AddProducts/AddProducts.jsx
+++ spacialbookingfront/src/Components/AddProducts/AddProducts.jsx
@@ -1,9 +1,33 @@
 import React from "react";
-import { icons } from "react-icons/lib";
-// import {  PlusOutlined  } from icons;
+// import { icons } from "react-icons/lib";
+import { MinusCircleOutlined, PlusOutlined  } from '@ant-design/icons';
 import { Button,Form,Input,InputNumber,Select } from "antd";
 import { useState } from "react";
 const {TextArea} = Input;
+
+
+
+
+
+
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 20 },
+  },
+};
+
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 20, offset: 4 },
+  },
+};
 
 
 
@@ -20,6 +44,73 @@ function AddProducts(){
         }}
         layout="horizontal"
       >
+
+
+<Form.List
+        name="names"
+        rules={[
+          {
+            validator: async (_, names) => {
+              if (!names || names.length < 2) {
+                return Promise.reject(new Error('At least 2 passengers'));
+              }
+            },
+          },
+        ]}
+      >
+        {(fields, { add, remove }, { errors }) => (
+          <>
+            {fields.map((field, index) => (
+              <Form.Item
+                {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                label={index === 0 ? 'Passengers' : ''}
+                required={false}
+                key={field.key}
+              >
+                <Form.Item
+                  {...field}
+                  validateTrigger={['onChange', 'onBlur']}
+                  rules={[
+                    {
+                      required: true,
+                      whitespace: true,
+                      message: "Please input passenger's name or delete this field.",
+                    },
+                  ]}
+                  noStyle
+                >
+                  <Input placeholder="passenger name" style={{ width: '60%' }} />
+                </Form.Item>
+                {fields.length > 1 ? (
+                  <MinusCircleOutlined
+                    className="dynamic-delete-button"
+                    onClick={() => remove(field.name)}
+                  />
+                ) : null}
+              </Form.Item>
+            ))}
+            <Form.Item>
+              <Button
+                type="dashed"
+                onClick={() => add()}
+                style={{ width: '60%' }}
+                icon={<PlusOutlined />}
+              >
+                Add field
+              </Button>
+              <Form.ErrorList errors={errors} />
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+
+
+
+
+
+
+
+
         <Form.Item label="Nombre">
           <Input />
         </Form.Item>
