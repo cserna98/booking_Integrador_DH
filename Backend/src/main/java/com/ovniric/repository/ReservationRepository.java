37351d8ecbac3118da6b8c4faf7106c57fdb568c
rociojulio <83599905+rociojulio@users.noreply.github.com>
2023-03-03 14:25:44 -0500

    cambios

--- Backend/src/main/java/com/ovniric/repository/ReservationRepository.java
+++ Backend/src/main/java/com/ovniric/repository/ReservationRepository.java
@@ -1,7 +1,11 @@
 package com.ovniric.repository;
 
 import com.ovniric.model.Reservation;
+import com.ovniric.model.user.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+//    List<Reservation> findByClient(Client client);
 }
