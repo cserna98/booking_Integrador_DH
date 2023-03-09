bd8d4ed0d7396afebf0cb7a726da7e6fb930ec95
rociojulio <83599905+rociojulio@users.noreply.github.com>
2023-03-09 14:20:37 -0500

    cambio

--- Backend/src/main/java/com/ovniric/config/SecurityConfiguration.java
+++ Backend/src/main/java/com/ovniric/config/SecurityConfiguration.java
@@ -25,10 +25,8 @@ public class SecurityConfiguration {
                 .csrf()//solicitudes necesitan enviar un token csrf
                 .disable()//desabilito la proteccion csrf
                 .authorizeHttpRequests()//reglas de autorización
-                .requestMatchers("/api/v1/auth/**", "/swagger-ui/**", "/v3/api-docs/**",
-                        "/swagger-ui.html", "/api/**")//todas las solicitudes que empiezan con este endpoint no necesitan ser autenticadas
+                .requestMatchers("/api/v1/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/api/**")//todas las solicitudes que empiezan con este endpoint no necesitan ser autenticadas
                 .permitAll()//todas las permitidas
-
                 .anyRequest() //request que necesitan ser autenticadas
                 .authenticated()//indica que necesitan ser autenticadas
                 .and()
